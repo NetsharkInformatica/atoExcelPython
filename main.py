e7d62ae79c0a63d19d2b71e916dfee00e76c8c08
Hélio Tomé <helio.tome2024@gmail.com>
2024-06-28 15:51:30 -0300

    finalizei a automação em Python com excel

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 import openpyxl
 from time import sleep
 
-
+paginaFecha=[]
 lePlan=openpyxl.load_workbook('dados_clientes.xlsx')
 paginaClientes=lePlan['Sheet1']
 driver=webdriver.Firefox()
@@ -16,6 +16,7 @@ for linha in paginaClientes.iter_rows(min_row=2,values_only=True):
     #input cpfInput
     sleep(3)
     campoPesquisa=driver.find_element(By.XPATH,"//input[@id='cpfInput']")
+    campoPesquisa.clear()
     sleep(1)
     campoPesquisa.send_keys(cpf)
     sleep(1)
@@ -24,8 +25,20 @@ for linha in paginaClientes.iter_rows(min_row=2,values_only=True):
     sleep(4)
     statusLabel=driver.find_element(By.XPATH,"//span[@id='statusLabel']")
     if statusLabel.text== 'em dia':
-        dataPag=driver.find_element(By.XPath,"//p[@id='paymentDate']")
-        metodoPag=driver.find_element(By.XPath,"//p[@id='paymentMethod']")
+        fechaPlani=openpyxl.load_workbook('planilha_fechamento.xlsx')
+        paginaFecha=fechaPlani['Sheet1']
+        dataPag=driver.find_element(By.XPATH,"//p[@id='paymentDate']")
+        metodoPag=driver.find_element(By.XPATH,"//p[@id='paymentMethod']")
+        sodata=dataPag.text.split()[3]
+        sometodo=metodoPag.text.split()[3]
+        
+        paginaFecha.append([nome,valor,cpf,vencimento,'em dia',sodata,sometodo])
+        fechaPlani.save('planilha_fechamento.xlsx')
     else:
+        fechaPlani=openpyxl.load_workbook('planilha_fechamento.xlsx')
+        paginaFecha=fechaPlani['Sheet1']
+        
+        paginaFecha.append([nome,valor,cpf,vencimento,'pendente'])
+        fechaPlani.save('planilha_fechamento.xlsx')
     
     
